f278ecb4fec69ed37c95b466a3dde8de4e88d70c
mirecodes <mirecodes@gmail.com>
2023-04-30 01:15:12 +0900

    FEAT: Add Routes into 3 pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import DisplayBillsContainer from './containers/DisplayBillsContainer';
 import DisplayUsersContainer from './containers/DisplayUsersContainer';
 import DisplayFormContainer from './containers/DisplayFormContainer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const App = () => {
-	return (
+const App = () => (
+	<BrowserRouter>
 		<div>
-			<DisplayFormContainer />
-			<hr />
-			<DisplayBillsContainer />
-			<hr />
-			<DisplayUsersContainer />
+			<Routes>
+				<Route path="/" element={<DisplayUsersContainer />} />
+				<Route path="/form" element={<DisplayFormContainer />} />
+				<Route path="/bills" element={<DisplayBillsContainer />} />
+			</Routes>
 		</div>
-	);
-};
+	</BrowserRouter>
+);
 
 export default App;
